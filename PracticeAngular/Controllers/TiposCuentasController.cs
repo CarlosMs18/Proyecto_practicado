d0660a79f1528ffcb256e3b7a9141a89e4a6a207
carlos <carlos.melgarejo.solorzano@hotmail.com>
2023-07-07 00:07:17 -0500

    e

--- PracticeAngular/Controllers/TiposCuentasController.cs
+++ PracticeAngular/Controllers/TiposCuentasController.cs
@@ -1,27 +1,74 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PracticeAngular.Models;
+using PracticeAngular.Servicios;
 
 namespace PracticeAngular.Controllers
 {
-    public class TiposCuentas : Controller
+    public class TiposCuentasController : Controller
     {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IRepositorioUsuario repositorioUsuario;
 
-        public IActionResult Index()
+        public TiposCuentasController(
+            IRepositorioTiposCuentas repositorioTiposCuentas,
+            IRepositorioUsuario repositorioUsuario
+            )
         {
-            return View();  
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.repositorioUsuario = repositorioUsuario;
         }
 
-        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = repositorioUsuario.obtenerUsuario();
+            var tiposCuentas = await repositorioTiposCuentas.ObtenerTiposCuentas(usuarioId);
+
+
+            return View(tiposCuentas);  
+        }
+
+       
         public IActionResult Crear()
         {
+            
 
             return View();
         }
 
 
         [HttpPost]
-        public IActionResult Crear(int xd)
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuentas)
         {
-            return View();
+            if(!ModelState.IsValid)
+            {
+                return View(tipoCuentas);
+            }
+
+            tipoCuentas.UsuarioId = repositorioUsuario.obtenerUsuario();
+
+            var existeTipoCuenta = await repositorioTiposCuentas.ExisteTipoCuenta(tipoCuentas.Nombre, tipoCuentas.UsuarioId);
+            if( existeTipoCuenta)
+            {
+                ModelState.AddModelError(nameof(tipoCuentas.Nombre), $"El nombre {tipoCuentas.Nombre} ya existe en la base de datos!");
+                return View(tipoCuentas);
+            }
+
+            await repositorioTiposCuentas.Crear(tipoCuentas);
+           
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ExisteTipoUsuario(string nombre)
+        {
+            var usuarioId = repositorioUsuario.obtenerUsuario();
+            var existeTipoCuenta = await repositorioTiposCuentas.ExisteTipoCuenta(nombre, usuarioId);
+            if (existeTipoCuenta)
+            {
+                return Json($"El nombre {nombre} ya existe en la base de datos!");
+            }
+            return Json(true);
+
         }
     }
 }
--- PracticeAngular/Servicios/RepositorioTiposCuentas.cs
+++ PracticeAngular/Servicios/RepositorioTiposCuentas.cs
@@ -6,7 +6,9 @@ namespace PracticeAngular.Servicios
 {
     public interface IRepositorioTiposCuentas
     {
-
+        Task Crear(TipoCuenta tipoCuenta);
+        Task<bool> ExisteTipoCuenta(string nombre, int usuarioId);
+        Task<IEnumerable<TipoCuenta>> ObtenerTiposCuentas(int usuarioId);
     }
 
     public class RepositorioTiposCuentas : IRepositorioTiposCuentas
@@ -15,14 +17,36 @@ namespace PracticeAngular.Servicios
 
         public RepositorioTiposCuentas(IConfiguration configuration)
         {
-            connectionString = configuration.GetConnectionString("DefaulConnection");
+            connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
-        public async Task Crear(TipoCuenta tipoCuenta)
+
+        public async Task<IEnumerable<TipoCuenta>> ObtenerTiposCuentas(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<TipoCuenta>(@"SELECT Id, Nombre, Orden
+                                                        FROM TiposCuenta
+                                                        WHERE UsuarioId = @UsuarioId
+                                                        ORDER BY Orden", new { usuarioId });
+        }
+            public async Task Crear(TipoCuenta tipoCuenta)
         {
             using var connection = new SqlConnection(connectionString);
             var id = await connection.QuerySingleAsync<int>(@"INSERT INTO TiposCuenta(Nombre, UsuarioId, Orden) 
-                                                            VALUES(@Nombre, @UsuarioId, 0);", tipoCuenta);
+                                                            VALUES(@Nombre, @UsuarioId, 0);
+                                                            SELECT SCOPE_IDENTITY()", tipoCuenta);
+
+            tipoCuenta.Id = id;
+        }
+
+
+        public async Task<bool> ExisteTipoCuenta(string nombre, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var existe = await connection.QueryFirstOrDefaultAsync<int>(@"
+                                                SELECT 1 FROM TiposCuenta 
+                                                WHERE Nombre = @Nombre AND UsuarioId = @UsuarioId;", new { nombre, usuarioId });
+            return existe == 1;
         }
     }
 }
